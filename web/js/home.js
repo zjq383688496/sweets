63fcb137321024825f1eab8c29716c69263805c6
庄家其 <zhuangjiaqi@cydeng.com>
2016-01-16 21:41:22 +0800

    003

---
+++ web/js/home.js
@@ -0,0 +1,15 @@
+$(function () {
+	//$('.brand-con').flexslider();
+	$('.flexslider').flexslider({
+		animation: "slide",
+		animationLoop: false,
+		slideshow: false,
+		itemWidth: 132,
+		itemMargin: 0,
+		minItems: 1,
+		maxItems: 7,
+		controlNav: false
+		//directionNav: false
+		// pausePlay: true
+	});
+});
